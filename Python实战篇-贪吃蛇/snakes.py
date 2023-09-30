c02a468dea2a559ddc34192958d5ab6604de8a14
Arror Chino <3262153358@qq.com>
2023-09-30 17:59:28 +0800

    Python实战篇-贪吃蛇_v1.0.2-2023-9-30 (注释下一版本更新)

--- Python实战篇-贪吃蛇/snakes.py
+++ Python实战篇-贪吃蛇/snakes.py
@@ -1,6 +1,8 @@
 import pygame as pygame
 import random
 import time
+import json
+import os
 pygame.init()
 
 #初始化一个clock变量，使用开头导入的time包。这个变量将用来处理游戏的帧率
@@ -42,34 +44,49 @@ dis.fill(background_color)
 clock = pygame.time.Clock()
 #分数设置
 score=0
-filename='score.txt'
+filename='score.json'
 
 def Your_score(score):
     value = score_font.render("Your Score: " + str(score), True, text_color)
     dis.blit(value, [0, 0])
     pygame.display.update()
 
-def write_score(content):
-    with open(filename,'w+',encoding='utf-8') as f:
-        s_old=f.readline(1)
-        if s_old:
-            if int(s_old)<content:
-                f.write(str(content))
-        else:
-            f.write(str(content))
+def save_score(content):
+    with open(filename,'r',encoding='utf-8') as fr:
+        #dic1=f.read()
+        s_old=json.load(fr)
+        s_old_lst=s_old['total']
+    with open(filename,'w+',encoding='utf-8') as fw:
+        fw.seek(0)
+        fw.truncate()
+        s_new_dic={'time':time.asctime(), 'score':content}
+        s_old_lst.append(s_new_dic)
+        dic={'total':s_old_lst}
+        
+        json.dump(dic,fw, indent=4, ensure_ascii=False) #fw为要写入的文件，dic是要写入的内容，后面部分用来格式化json
+        fw.close()
 
 def read_score():
-    with open(filename,'r',encoding='utf-8') as f:
-        s_high=f.readline()
-        if s_high:
-            display_message(s_high, text_color, (550,30) )
-            return int(s_high)
+    global highest_score
+    s_lst=[]
+    highest_score='0'
+    with open(filename,'r',encoding='utf-8') as file:
+        try:
+            s_old=json.load(file)
+            if s_old:
+                for dic in s_old['total']:
+                    s_lst.append(dic['score'])
+                    s_lst.sort(reverse=True) #降序排序
+                    highest_score=str( s_lst[0] )
+        except:
+            pass
+            '''display_message(s_high, text_color, (550,30) )'''
 
 #显示文字
 def display_message(text,color,position):
-    text1=font_style.render(text,True,color)
+    text1=score_font.render(text,True,color)
     dis.blit(text1,position)
-    '''pygame.display.update()'''
+    pygame.display.update()
 
 #画边线
 def draw_box():
@@ -104,8 +121,11 @@ def snake_move(origin_direction):
         #调用完此函数后要移除蛇尾
 
 snake_length=len(snake_body)
+
 def run():
     global snake_length, score
+    check_file(filename)
+    read_score()
     game_close=False
     game_over=False
     dire='direction_right'
@@ -140,6 +160,7 @@ def run():
             draw_box()
             draw_snake(food_position)
             Your_score(score)
+            show_highest_score()
             pygame.display.flip()
             
             for x in snake_body[1::1]: #反向遍历
@@ -147,20 +168,20 @@ def run():
                     game_over = True
             
             clock.tick(snake_speed)
-        while game_over==True:
+        if game_over==True:
             dis.fill(background_color)
             display_message("Lost! q quit or p again", text_color, [window_width / 6, window_high / 3])
             Your_score(snake_length - 2 )
-            write_score(score)
-
-            for event in pygame.event.get():
-                if event.type==pygame.KEYDOWN:
-                    if event.key==pygame.K_q:
-                        game_over=False
-                        game_close=True
-                    elif event.key==pygame.K_p:
-                        startup()
-                        run()
+            save_score(score)
+            while game_over==True:
+                for event in pygame.event.get():
+                    if event.type==pygame.KEYDOWN:
+                        if event.key==pygame.K_q:
+                            game_over=False
+                            game_close=True
+                        elif event.key==pygame.K_p:
+                            startup()
+                            run()
     pygame.quit()
     quit()
 
@@ -174,6 +195,27 @@ def startup():
     score=0
     snake_length=len(snake_body)
     read_score()
+
+def check_file(f_name): #读取json文件
+    dic1={"total":[{"time":"1","score":0}]}
+    if os.path.exists(f_name): #检测文件是否存在
+        try:
+            with open(f_name,'r',encoding='utf-8') as rfile:
+                json.load(rfile) #尝试读取json文件
+        except:
+            with open(f_name,'w+',encoding='utf-8') as wfile:
+                wfile.seek(0)
+                wfile.truncate()
+                json.dump(dic1, wfile)
+                wfile.close()
+    else:
+        with open(f_name,'x',encoding='utf-8') as xfile: #'x'指创建一个新文件，并进行写入
+            json.dump(dic1, xfile)
+            xfile.close()
+
+def show_highest_score():
+    global highest_score
+    display_message('Highest Score: '+highest_score, text_color, (360,0) )
 
 run()
             
